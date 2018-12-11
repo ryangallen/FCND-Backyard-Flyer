fad3f9b943acd0e94fed1a966f18842ba231aabb
Ryan Allen <allenryan14@gmail.com>
2018-12-10 20:28:10 -0500

    clean start

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -1,4 +1,3 @@
-import argparse
 import time
 from enum import Enum
 
@@ -27,10 +26,8 @@ class BackyardFlyer(Drone):
         self.in_mission = True
         self.check_state = {}
 
-        # initial state
         self.flight_state = States.MANUAL
 
-        # TODO: Register all your callbacks here
         self.register_callback(MsgID.LOCAL_POSITION, self.local_position_callback)
         self.register_callback(MsgID.LOCAL_VELOCITY, self.velocity_callback)
         self.register_callback(MsgID.STATE, self.state_callback)
@@ -61,14 +58,14 @@ class BackyardFlyer(Drone):
 
     def calculate_box(self):
         """TODO: Fill out this method
-        
+
         1. Return waypoints to fly a box
         """
         pass
 
     def arming_transition(self):
         """TODO: Fill out this method
-        
+
         1. Take control of the drone
         2. Pass an arming command
         3. Set the home location to current position
@@ -78,7 +75,7 @@ class BackyardFlyer(Drone):
 
     def takeoff_transition(self):
         """TODO: Fill out this method
-        
+
         1. Set target_position altitude to 3.0m
         2. Command a takeoff to 3.0m
         3. Transition to the TAKEOFF state
@@ -87,7 +84,7 @@ class BackyardFlyer(Drone):
 
     def waypoint_transition(self):
         """TODO: Fill out this method
-    
+
         1. Command the next waypoint position
         2. Transition to WAYPOINT state
         """
@@ -95,7 +92,7 @@ class BackyardFlyer(Drone):
 
     def landing_transition(self):
         """TODO: Fill out this method
-        
+
         1. Command the drone to land
         2. Transition to the LANDING state
         """
@@ -103,50 +100,30 @@ class BackyardFlyer(Drone):
 
     def disarming_transition(self):
         """TODO: Fill out this method
-        
+
         1. Command the drone to disarm
         2. Transition to the DISARMING state
         """
         print("disarm transition")
 
     def manual_transition(self):
-        """This method is provided
-        
-        1. Release control of the drone
-        2. Stop the connection (and telemetry log)
-        3. End the mission
-        4. Transition to the MANUAL state
-        """
         print("manual transition")
-
         self.release_control()
         self.stop()
         self.in_mission = False
         self.flight_state = States.MANUAL
 
     def start(self):
-        """This method is provided
-        
-        1. Open a log file
-        2. Start the drone connection
-        3. Close the log file
-        """
-        print("Creating log file")
         self.start_log("Logs", "NavLog.txt")
+
         print("starting connection")
-        self.connection.start()
-        print("Closing log file")
+        super().start()
+
         self.stop_log()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=5760, help='Port number')
-    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
-    args = parser.parse_args()
-
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
-    #conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
-    drone = BackyardFlyer(conn)
+    connection = MavlinkConnection('tcp:127.0.0.1:5760', threaded=False, PX4=False)
+    drone = BackyardFlyer(connection)
     time.sleep(2)
     drone.start()
